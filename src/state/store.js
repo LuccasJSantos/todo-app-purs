24daf848e7f08286944d8cd8c1849c585e26c297
LuccasJSantos <luccasjsantos@hotmail.com>
2021-09-11 01:58:38 -0300

    Change static state return when useStore is invoked

--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,11 @@
 import todos from './Todos'
 
+const states = {
+  todos: todos.state
+}
+
 const actions = {
   todos: todos.actions
 }
 
-export const useStore = (name) => [todos.state, actions[name]]
+export const useStore = (name) => [states[name], actions[name]]
