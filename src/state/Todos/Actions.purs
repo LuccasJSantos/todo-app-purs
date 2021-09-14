f143330c3cfe987b01f4f185ca9dc2d4e7b00eac
LuccasJSantos <luccasjsantos@hotmail.com>
2021-09-14 00:47:01 -0300

    Refactor for a more concise purs way
    Since I learned some more shenanigans

--- src/state/Todos/Actions.purs
+++ src/state/Todos/Actions.purs
@@ -7,19 +7,20 @@ import Data.Array (filter, snoc)
 import State.Todos (Todo, State)
 
 addTodo :: State -> String -> State
-addTodo state title = {
-    nextId: add state.nextId 1,
-    todos: snoc state.todos newTodo
+addTodo state title =
+  { nextId: add state.nextId 1
+  , todos: snoc state.todos newTodo
   }
   where
     newTodo :: Todo
-    newTodo = { id: state.nextId, title: title, done: false }
+    newTodo =
+      { id: state.nextId
+      , title: title
+      , done: false
+      }
 
 removeTodo :: State -> Int -> State
-removeTodo state id = {
-    nextId: state.nextId,
-    todos: filter outId state.todos
-  }
+removeTodo state id = state { todos = filter outId state.todos }
   where
     outId :: Todo -> Boolean
-    outId todo = todo.id /= id
+    outId todo = notEq todo.id id
