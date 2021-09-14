068ba5dc4c37d041342521edc06f5b67acbed4fd
LuccasJSantos <luccasjsantos@hotmail.com>
2021-09-13 23:37:31 -0300

    Remove previous action.js file

--- src/state/Todos/actions-before.js
+++
@@ -1,7 +0,0 @@
-export const addTodo = (stateRef, title) => {
-  return [...stateRef, { title, done: false }]
-}
-
-export const removeTodo = (stateRef, index) => {
-  return [...stateRef.slice(0, index), ...stateRef.slice(index + 1)]
-}
